a56a8b91916f02f0cca7f1a8bb30c5f17c780944
MattJWS <majustus@senators.ws.edu>
2024-05-03 19:43:48 -0400

    Added BookViewModel to models

---
+++ BookDatabase/BookDatabase/Models/BookViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace BookDatabase.Models
+{
+    public class BookViewModel
+    {
+        public Book Book { get; set; } = null!;
+        public List<Author> Authors { get; set; } = null!;
+        public List<Book> Books { get; set; } = null!;
+        public List<BookAuthor> BookAuthors { get; set; } = null!;
+        public List<Genre> Genres { get; set; } = null!;
+        public List<Status> Statuses { get; set; } = null!;
+    } // end view model
+} // end namespace
