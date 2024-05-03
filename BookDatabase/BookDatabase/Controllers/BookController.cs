aa96f13c9fc72ab002238f528ef3a1887b82fb13
MattJWS <majustus@senators.ws.edu>
2024-05-03 19:33:18 -0400

    Added book controller

---
+++ BookDatabase/BookDatabase/Controllers/BookController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookDatabase.Controllers
+{
+    public class BookController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        } // end action
+    } // end book controller
+} // end namespace
