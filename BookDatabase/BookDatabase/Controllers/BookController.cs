03952622dabe54cfea32b59b2e3c59eb31bca9d4
MattJWS <majustus@senators.ws.edu>
2024-05-04 17:52:06 -0400

    Added the AddCard model and action methods

--- BookDatabase/BookDatabase/Controllers/BookController.cs
+++ BookDatabase/BookDatabase/Controllers/BookController.cs
@@ -20,5 +20,20 @@ namespace BookDatabase.Controllers
             };
             return View(model);
         } // end action
+
+        [HttpGet]
+        public IActionResult AddBook()
+        {
+            return View(new Book());
+        } // end action AddBook
+
+        [HttpPost]
+        public IActionResult AddBook(Book book)
+        {
+            // add new book and authors to the database
+            // new authors might have to be added first then their
+            // author Ids can be added to the BookAuthors table.
+            return View();
+        } // end action AddBook
     } // end book controller
 } // end namespace
