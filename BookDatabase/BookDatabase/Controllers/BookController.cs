bd76e1a7e6f25945f92d4102ec2fefd400506fe0
MattJWS <majustus@senators.ws.edu>
2024-05-12 18:23:27 -0400

    All CRUD operations working for Books and Authors

--- BookDatabase/BookDatabase/Controllers/BookController.cs
+++ BookDatabase/BookDatabase/Controllers/BookController.cs
@@ -71,13 +71,61 @@ namespace BookDatabase.Controllers
         [HttpGet]
         public IActionResult EditBook(int id)
         {
-            var book = Context.Books.Find(id);
-            return View(book);
+            var model = new BookViewModel()
+            {
+                Book = Context.Books.Find(id),
+                Authors = Context.Authors.OrderBy(a => a.AuthorName).ToList(),
+                Genres = Context.Genres.OrderBy(g => g.Name).ToList(),
+                Statuses = Context.Statuses.OrderBy(s => s.StatusName).ToList()
+            };
+            return View(model);
         } // end edit book action
+
+        [HttpPost]
+        public IActionResult EditBook(BookViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Context.Books.Update(model.Book);
+                Context.SaveChanges();
+
+                var bookAuthors = Context.BookAuthors;
+                foreach(var ba in bookAuthors)
+                {
+                    if (ba.BookId == model.Book.BookId)
+                    {
+                        Context.Remove(ba);
+                        Context.SaveChanges();
+                    } // end if 
+                } // end foreach
+
+                //Add the new records to bookAuthor table
+                foreach (int authorId in model.SelectedAuthorIds)
+                {
+                    var newBook = new BookAuthor() { BookId = model.Book.BookId, AuthorId = authorId };
+                    Context.BookAuthors.Add(newBook);
+                    Context.SaveChanges();
+                }
+
+                return RedirectToAction("Index", "Book");
+            }
+            else
+            {
+                var newModel = new BookViewModel()
+                {
+                    Book = model.Book,
+                    Authors = Context.Authors.OrderBy(a => a.AuthorName).ToList(),
+                    Genres = Context.Genres.OrderBy(g => g.Name).ToList(),
+                    Statuses = Context.Statuses.OrderBy(s => s.StatusName).ToList()
+                };
+                return View(newModel);
+            }
+        }
 
         [HttpGet]
         public IActionResult DeleteBook(int id)
         {
+
             var book = Context.Books.Find(id);
             return View(book);
         } // end action delete book
@@ -89,9 +137,11 @@ namespace BookDatabase.Controllers
             Context.SaveChanges();
             return RedirectToAction("Index", "Book");
         } // end action post delete book
-        //----------------------------- End actions for Book ----------------------
+        //----------------------------- End actions for Book ---------------------//
 
-        //----------------------------- Actions for Author -----------------------
+        //////////////////////////////////////////////////////////////////////////
+
+        //----------------------------- Actions for Author -----------------------//
         public IActionResult AuthorList()
         {
             var model = Context.Authors.OrderBy(a => a.AuthorName).ToList();
@@ -117,6 +167,46 @@ namespace BookDatabase.Controllers
             }            
             return RedirectToAction("AuthorList", "Book");
         } // end action AddAuthor Post
+
+        [HttpGet]
+        public IActionResult EditAuthor(int id)
+        {
+            var author = Context.Authors.Find(id);
+            return View(author);
+        }
+
+        [HttpPost]
+        public IActionResult EditAuthor(Author author)
+        {
+            if (ModelState.IsValid) { 
+                Context.Authors.Update(author);
+                Context.SaveChanges();
+                
+            } else
+            {
+                return View(author);
+            }
+            return RedirectToAction("AuthorList", "Book");
+        }
+
+        [HttpGet]
+        public IActionResult DeleteAuthor(int id)
+        {
+            var author = Context.Authors.Find(id);
+            return View(author);
+        } // end delete author get action
+
+        [HttpPost] 
+        public IActionResult DeleteAuthor(Author author) 
+        {
+            Context.Authors.Remove(author);
+            Context.SaveChanges();
+            return RedirectToAction("AuthorList", "Book");
+        
+        } // end delete author post action
+
+
+
     //------------------------------- End actions for Author---------------------
     } // end book controller
 } // end namespace
--- BookDatabase/BookDatabase/obj/Debug/net8.0/BookDatabase.AssemblyInfo.cs
+++ BookDatabase/BookDatabase/obj/Debug/net8.0/BookDatabase.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookDatabase")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d9c593774f481561b606183323b673139eb0894")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df0a6bcfcff146548fee3b443c14353bddf95785")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookDatabase")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookDatabase")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
